77b0073238273c97f4c70ad3234f3108640403da
resfer3 <resfer333@gmail.com>
2024-08-14 04:52:22 -0600

    assignment2 done

---
+++ Assignment-02/assignment2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+
+
+int main(void){
+
+    int n = 3;
+    float f = 3.14;
+    char* s = "Hello, World!";
+
+    printf("%i is an integer\n", n);
+    printf("%f is a float\n", f);
+    printf("%s is a char\n", s);
+
+}
