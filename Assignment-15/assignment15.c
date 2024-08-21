ffad298e2a4a9bc4397c6a3a39521984403e3a0e
resfer3 <resfer333@gmail.com>
2024-08-21 05:20:52 -0600

    Assignment 15 done, stopping a little bit to study and experiment a little more about pointers

--- Assignment-15/assignment15.c
+++ Assignment-15/assignment15.c
@@ -7,6 +7,11 @@ int main(void){
   int *ptr = &variable;
   
   // outputs
-  printf(
+  printf("The value of int variable is: %d\n", variable);
+  printf("The value of the pointer to the int variable is: %p\n", ptr);
+  printf("The memory address of the int variable is %p\n", &variable);
+  printf("The value held at the memory location that the pointer is pointing to is %d\n", *ptr);
+
+  return 0;
 
 }
