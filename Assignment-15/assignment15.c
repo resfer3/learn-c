a4b6ffacfb98a437ef0a7b1a876e52a5ff7a249d
resfer3 <resfer333@gmail.com>
2024-08-23 04:38:24 -0600

    modified

--- Assignment-15/assignment15.c
+++ Assignment-15/assignment15.c
@@ -5,12 +5,19 @@ int main(void){
   // init var
   int variable = 10;
   int *ptr = &variable;
+
+  // other ways
+  // second_ptr pointing to the same variable
+  int *second_ptr = ptr;
   
   // outputs
   printf("The value of int variable is: %d\n", variable);
-  printf("The value of the pointer to the int variable is: %p\n", ptr);
+  printf("The value of the pointer to the int variable is: %d\n", *second_ptr);
   printf("The memory address of the int variable is %p\n", &variable);
   printf("The value held at the memory location that the pointer is pointing to is %d\n", *ptr);
+
+  int *second_ptr_address = ptr;
+  printf("The memory address of the second_ptr is: %p\n", &second_ptr_address);
 
   return 0;
 
