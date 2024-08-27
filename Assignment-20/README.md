520be2ef210c62d2e40dfd932eec017ee3724b50
resfer3 <resfer333@gmail.com>
2024-08-26 19:32:30 -0600

    finished assignment20, added README

---
+++ Assignment-20/README.md
@@ -0,0 +1,4 @@
+# Assignment 20
+Create/define a structure (struct) outside of main() that includes an int, char, and float member. Inside of main(), declare values for those struct members and then print their values to the terminal.
+
+Assignment [here](https://github.com/h0mbre/Learning-C/tree/master/Assignment-20)
--- Assignment-20/assignment20.c
+++ Assignment-20/assignment20.c
@@ -1,18 +1,31 @@
 #include <stdio.h>
 
+// One Way
 struct types{
   int a;
   char b;
   float c;
-}types;
+};
+
+// Second Way (typedef)
+typedef struct {
+  int x;
+  int y;
+  float ap;
+} types2;
 
 int main(void){
 
-  types.a = 3;
-  types.b = 'W';
-  types.c = 3.14;
+  // One way
+  // assign values to struct types
+  struct types var = { 3, 'Q' , 3.15 }; 
 
-  printf("This is the int: %d, this is the char: %c, this is the float: %f\n", types.a, types.b, types.c);
+  // Second Way (typedef)
+  types2 var2 = { 3, 1, 10.10 };
+
+  printf("This is the int: %d, this is the char: %c, this is the float: %f\n", var.a, var.b, var.c);
+  printf("This is the typedef way\n");
+  printf("This is the int: %d, this is the int: %d, this is the float: %f\n", var2.x, var2.y, var2.ap);
 
   return 0;
 
